21a05bf1c898c5f1115370dda9dd92f984f6f081
ANDRES KWAN <andreskwan@gmail.com>
2013-05-06 14:32:28 -0500

    agbo-ex2 - KWCharacterModel - designated and convenient initializers

--- StarWars/model/KWCharacterModel.h
+++ StarWars/model/KWCharacterModel.h
@@ -10,4 +10,28 @@
 
 @interface KWCharacterModel : NSObject
 
+@property (strong, nonatomic) NSString *firstName;
+@property (strong, nonatomic) NSString *lastName;
+@property (strong, nonatomic) NSString *alias;
+//how to access to a local/remote resource
+@property (strong, nonatomic) NSURL    *wikiPage;
+//to read a binary file, any data
+@property (strong, nonatomic) NSData   *soundData;
+//manipulate but do not display
+@property (strong, nonatomic) UIImage  *photo;
+
+-(id) initWithFirstName: (NSString *) aFirstName
+               lastName: (NSString *) aLastName
+                  alias: (NSString *) anAlias
+               wikiPage: (NSURL *) aWikiPage
+              soundData: (NSData *) aSoundData
+                  photo: (UIImage *) aPhoto;
+
+//ctrl + i to indent
+-(id) initWithAlias: (NSString *) anAlias
+           wikiPage: (NSURL *) aWikiPage
+          soundData: (NSData *) aSoundData
+              photo: (UIImage *) aPhoto;
+
+
 @end
