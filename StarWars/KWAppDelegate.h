dec0b47c551758894ee9049e3a927349f87cabc0
ANDRES KWAN <andreskwan@gmail.com>
2013-05-06 13:38:28 -0500

    Initial Commit

---
+++ StarWars/KWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KWAppDelegate.h
+//  StarWars
+//
+//  Created by Andres Kwan on 5/6/13.
+//  Copyright (c) 2013 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
